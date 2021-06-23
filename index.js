fd1f4ef7046e8aaf4d33d35204d77fd6f45389be
Andrew Kostin <ambrraxia@gmail.com>
2021-06-23 23:32:31 +0300

    Add password generation

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-alert(CryptoJS.SHA256('test'))
+const SYMBOLS_MAP = {
+  a: '@',
+  b: '#',
+  c: '!',
+  d: '%',
+  e: ')',
+  f: '(',
+}
+
+function getElements() {
+  const secretElement = document.getElementById('secret')
+  const masterElement = document.getElementById('master')
+  const serviceElement = document.getElementById('service')
+  const resultForm = document.getElementById('form_result')
+  const resultElement = document.getElementById('result')
+
+  return [secretElement, masterElement, serviceElement, resultForm, resultElement]
+}
+
+function showResult(password) {
+  const elements = getElements()
+  elements[3].style.display = 'block'
+  elements[4].innerText = password
+}
+
+function generatePassword() {
+  const [secretElement, masterElement, serviceElement] = getElements()
+
+  const secret = secretElement.value
+  const master = masterElement.value
+  const service = serviceElement.value
+
+  if (!secret || !master || !service) {
+    alert('All fields are required!')
+    return
+  }
+
+  const secretSHA = CryptoJS.SHA256(secret)
+  const masterSHA = CryptoJS.SHA256(master)
+  const serviceSHA = CryptoJS.SHA256(service)
+
+  const general = `${secret}-${secretSHA}-${master}-${masterSHA}-${service}-${serviceSHA}`
+  const generalSHA = CryptoJS.SHA256(general).toString()
+
+  return Array.from(generalSHA)
+    .reduce((acc, current, index) => {
+      if (index % 4 === 0) {
+        return [...acc, current]
+      }
+      return acc
+    }, [])
+    .reduce((acc, current, index) => {
+      if (index <7 && index % 2 === 0) {
+        return [...acc, current.toUpperCase()]
+      }
+      return [...acc, current]
+    }, [])
+    .reduce((acc, current, index) => {
+      if (index > 7) {
+        return [...acc, SYMBOLS_MAP[current] || current]
+      }
+      return [...acc, current]
+    }, [])
+    .join('')
+}
+
+function onClick() {
+  const password = generatePassword()
+
+  if (password) {
+    showResult(password)
+  }
+}
+
+const generate = document.getElementById('generate')
+generate.addEventListener('click', onClick)
